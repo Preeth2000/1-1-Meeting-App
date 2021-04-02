99c689bfc7771e56077490dce4de80a706ba4668
Preeth <selvamop@uni.coventry.ac.uk>
2021-04-02 18:56:48 +0100

    Added user details to be shown during call

--- app/src/main/java/com/example/videocallapp/firebase/MessagingService.java
+++ app/src/main/java/com/example/videocallapp/firebase/MessagingService.java
@@ -1,9 +1,12 @@
 package com.example.videocallapp.firebase;
 
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.example.videocallapp.activities.Send_Invite;
+import com.example.videocallapp.utilities.Constants;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -28,6 +31,20 @@ public class MessagingService extends FirebaseMessagingService {
         }
 
          */
+
+        String type = remoteMessage.getData().get(Constants.REMOTE_MSG_TYPE);
+        if(type != null) {
+            if(type.equals(Constants.REMOTE_MSG_INVITATION)){
+                Intent intent = new Intent(getApplicationContext(), Send_Invite.class);
+                intent.putExtra(Constants.REMOTE_MSG_MEETING_TYPE, remoteMessage.getData().get(Constants.REMOTE_MSG_MEETING_TYPE));
+                intent.putExtra(Constants.KEY_FIRST_NAME, remoteMessage.getData().get(Constants.KEY_FIRST_NAME));
+                intent.putExtra(Constants.KEY_LAST_NAME, remoteMessage.getData().get(Constants.KEY_LAST_NAME));
+                intent.putExtra(Constants.KEY_EMAIL, remoteMessage.getData().get(Constants.KEY_EMAIL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+
+            }
+        }
 
     }
 }
