2fc9a144121fdb4f934a1052cc4049ea4dd2ceee
Preeth <selvamop@uni.coventry.ac.uk>
2021-04-02 18:56:04 +0100

    Added store for data from FireStore database, Included server key

--- app/src/main/java/com/example/videocallapp/utilities/Constants.java
+++ app/src/main/java/com/example/videocallapp/utilities/Constants.java
@@ -1,5 +1,7 @@
 package com.example.videocallapp.utilities;
 
+import java.util.HashMap;
+
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_FIRST_NAME = "first_name";
@@ -11,4 +13,23 @@ public class Constants {
 
     public static final String KEY_PREFERENCE_NAME = "videoMeetingPreference";
     public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+
+    //To store data from FireStore database
+
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+
+    public static final String REMOTE_MSG_TYPE = "type";
+    public static final String REMOTE_MSG_INVITATION = "invitation";
+    public static final String REMOTE_MSG_MEETING_TYPE = "meetingType";
+    public static final String REMOTE_MSG_INVITER_TOKEN ="inviterToken";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> getRemoteMessageHeaders() {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put(Constants.REMOTE_MSG_AUTHORIZATION, "key=AAAAFYRCVD0:APA91bEiJbr_9Cj0sIbY7OljYl5-LPdAXhY-MlumlMBF6n2Qm-OyGuFIII1cmdBOsFXYzipGZFObQQlRgRwq1IaamJTWjRv8xvYx4m5O18QNa8wuDsdJJaomZOMWoYUIyLeUBk_MlFbq");
+        headers.put(Constants.REMOTE_MSG_CONTENT_TYPE, "application/json");
+        return headers;
+    }
 }
